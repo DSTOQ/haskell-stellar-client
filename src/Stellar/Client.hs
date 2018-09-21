075b7dac2e6a23e237784bee7caa505a803dfdc7
Yuriy Lazarev <Unisay@users.noreply.github.com>
2018-09-21 17:32:36 +0200

    re-export Stellar

--- src/Stellar/Client.hs
+++ src/Stellar/Client.hs
@@ -1,9 +1,11 @@
 module Stellar.Client
-  ( module Stellar.Client.Types
+  ( module Stellar
+  , module Stellar.Client.Types
   , module Control.Monad.Stellar
   , module Control.Monad.Trans.Stellar
   ) where
 
 import Control.Monad.Stellar
 import Control.Monad.Trans.Stellar
+import Stellar
 import Stellar.Client.Types
